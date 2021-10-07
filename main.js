4e123b6fcdf22bdae4d58a05e9b99078cbf7c8b8
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-10-07 11:47:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function setup(){
     canavs.center()
     video = createCapture(VIDEO);
     video.hide();
-    classifier=ml5.imageClassifier('')
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/H1-qeSZzg/model.json')
 }
 function draw(){
     image(video,0,0,300,300);
